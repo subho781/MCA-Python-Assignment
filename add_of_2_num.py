6d56e4eb6d507b4f11ba0ca1e4fa297c999180fa
Subhajit Sarkar <subhajitsarkar148@gmail.com>
2021-01-28 23:52:09 +0530

    Delete add_of_2_num.py

--- add_of_2_num.py
+++
@@ -1,5 +0,0 @@
-#this program demonstrates the addition of the two numbers
-n1 = float (input ('enter the first number: '))
-n2 = float (input ('enter the second number: '))
-add = n1 +n2
-print('the sum is: ',add)
