abe9dafa9730d190cdb4b93e68c51c1edd414ff9
Subhajit Sarkar <subhajitsarkar148@gmail.com>
2021-01-29 00:15:57 +0530

    update radius.py

---
+++ Radius.py
@@ -0,0 +1,5 @@
+#program to calculate the area of the circle take radius as user input 
+pi = 3.14 #initializing the value of pi
+radius = float(input("Enter the radius: "))
+area = pi * (radius**2) #calculating the area 
+print("The area of the circle is", area ) 
