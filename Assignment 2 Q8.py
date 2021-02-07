bebdd42b0d5110230b1916cb481af502a9039237
Subhajit Sarkar <subhajitsarkar148@gmail.com>
2021-02-07 23:02:46 +0530

    update

--- Assignment 2 Q8.py
+++ Assignment 2 Q8.py
@@ -4,7 +4,8 @@ sub2=int(input("Enter marks of the second subject: "))
 sub3=int(input("Enter marks of the third subject: "))
 sub4=int(input("Enter marks of the fourth subject: "))
 sub5=int(input("Enter marks of the fifth subject: "))
-avg=(sub1+sub2+sub3+sub4+sub4)/5
+total=(sub1+sub2+sub3+sub4+sub4)
+avg=total/5
 if(avg>=90):
     print("Grade: O")
 elif(avg>=80 and avg<=89):
