3fc70f8f249ea14c2dfc0966e97070f93df1ccc9
Subhajit Sarkar <subhajitsarkar148@gmail.com>
2021-02-07 22:08:24 +0530

    update Assignment 2 Q2.py

---
+++ Assignment 2 Q2.py
@@ -0,0 +1,11 @@
+# Python Program to find Largest of Two Numbers
+
+a = float(input(" Please Enter the First number : "))
+b = float(input(" Please Enter the Second number : "))
+
+if(a > b):
+    print('first number is lergest ')
+elif(b > a):
+    print('second number is lergest')
+else:
+    print("Both are Equal")
