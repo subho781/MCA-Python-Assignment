c73e781b6944809652fc5eb65483ac89e53e29a1
Subhajit Sarkar <subhajitsarkar148@gmail.com>
2021-02-07 23:10:31 +0530

    update

---
+++ Assignment 2 Q10.py
@@ -0,0 +1,12 @@
+#WAP to input 3 side of a triangle and prints its type (i.e Equilateral/Isosceles/Scalene).
+print("Input lengths of the triangle sides: ")
+x = int(input("x: "))
+y = int(input("y: "))
+z = int(input("z: "))
+
+if x == y == z:
+	print("Equilateral triangle")
+elif x==y or y==z or z==x:
+	print("isosceles triangle")
+else:
+	print("Scalene triangle")
